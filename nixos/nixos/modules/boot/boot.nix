9cc196690b1cbd93ba145b5452cf10fb56f64077
LGBaracco <lorenzobaracco01@gmail.com>
2026-08-28 21:35:03 +0200

    restored early numlock for luks password

--- nixos/nixos/modules/boot/boot.nix
+++ nixos/nixos/modules/boot/boot.nix
@@ -30,26 +30,25 @@
       "rd.udev.log_level=3"
       "rd.systemd.show_status=auto"
     ];
-    # TODO maybe delete altogether
     # # Enable numlock early on boot
-    # initrd.systemd = {
-    #   storePaths = [
-    #     "${pkgs.kbd}/bin/setleds"
-    #   ];
-    #   services.numlockon = {
-    #     description = "Enable NumLock at startup";
-    #     wantedBy = [ "initrd.target" ];
-    #     before = [ "initrd-root-device.target" ];
-    #     unitConfig = {
-    #       DefaultDependencies = false;
-    #     };
-    #     serviceConfig = {
-    #       Type = "oneshot";
-    #       ExecStart = "${pkgs.kbd}/bin/setleds -D +num";
-    #       StandardInput = "tty";
-    #       TTYPath = "/dev/tty0";
-    #     };
-    #   };
-    # };
+    initrd.systemd = {
+      storePaths = [
+        "${pkgs.kbd}/bin/setleds"
+      ];
+      services.numlockon = {
+        description = "Enable NumLock at startup";
+        wantedBy = [ "initrd.target" ];
+        before = [ "initrd-root-device.target" ];
+        unitConfig = {
+          DefaultDependencies = false;
+        };
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.kbd}/bin/setleds -D +num";
+          StandardInput = "tty";
+          TTYPath = "/dev/tty0";
+        };
+      };
+    };
   };
 }

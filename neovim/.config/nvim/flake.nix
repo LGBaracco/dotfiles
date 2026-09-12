2db3920a565f123363bca0af9caf7678f009c2ab
LGBaracco <lorenzobaracco01@gmail.com>
2026-09-13 00:10:17 +0200

    nuked noctalia

--- neovim/.config/nvim/flake.nix
+++ neovim/.config/nvim/flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Neovim config (nix-wrapper-modules module)";
   outputs = { self }: {
-    homeModules.neovim = "${self}/.config/nvim/module.nix";
+    homeModules.neovim = "${self}/module.nix";
   };
 }
